bb08279daf1c3d4384142dc1212fdae50357513d
Coffee <118107110+KahveSever@users.noreply.github.com>
2022-11-13 14:51:14 +0300

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,17 @@
+greeting = 'Hello'
+name = 'Akın'
+
+# print(dir(name))
+#komutlar bulmak için kullanılıyor sanırım
+
+# print(help(str)) #str = string'in kısaltılmış hali
+#üstteki komut stringlerin ne işe yardığını ve neler yapılacağı hakkında ingilizce metin var.
+print(help(str.lower))
+
+"""
+Ekran çıktısı:
+Help on method_descriptor:
+
+lower(self, /)
+    Return a copy of the string converted to lowercase.
+"""
